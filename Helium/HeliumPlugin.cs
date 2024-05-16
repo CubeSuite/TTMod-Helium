7c2198e57ea889402f163b37ed7c2cd33b0b1439
CubeSuite <cubesuite@outlook.com>
2024-05-16 08:53:00 +0100

    Add project files.

---
+++ Helium/HeliumPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Helium.Patches;
+using UnityEngine;
+
+namespace Helium
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class HeliumPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.Helium";
+        private const string PluginName = "Helium";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+
+            Harmony.CreateAndPatchAll(typeof(VOPlaybackManagerPatch));
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+    }
+}
---
+++ Helium/Patches/VOPlaybackManagerPatch.cs
@@ -0,0 +1,19 @@
+﻿using FMOD.Studio;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helium.Patches
+{
+    public class VOPlaybackManagerPatch
+    {
+        [HarmonyPatch(typeof(VOPlaybackManager), "GetDialogueEventInstance")]
+        [HarmonyPostfix]
+        static void TurnUpPitch(ref EventInstance __result) {
+            __result.setPitch(1.5f);
+        }
+    }
+}
